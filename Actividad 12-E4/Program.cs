ff30ce2f740cc9994305bb0b76ae8fe4c4f6de34
erick <erick@LEGI0SORIO>
2026-02-22 20:33:25 -0600

    “Proyecto creado”

--- Actividad 12-E4/Program.cs
+++ Actividad 12-E4/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿Console.WriteLine("MENU");
+Console.WriteLine("1. Malware");
+Console.WriteLine("2. Phishing");
+Console.WriteLine("3. Acceso no autorizado");
+Console.WriteLine("4. Fuga de información");
+Console.WriteLine("Seleccionar una opción: ");
+int opcion = int.Parse(Console.ReadLine());
