eaeea1a24f3e135700d06cb59a7ec355c5534a44
erick <erick@LEGI0SORIO>
2026-02-22 20:39:15 -0600

    “Switch implementado”

--- Actividad 12-E4/Program.cs
+++ Actividad 12-E4/Program.cs
@@ -5,3 +5,151 @@ Console.WriteLine("3. Acceso no autorizado");
 Console.WriteLine("4. Fuga de información");
 Console.WriteLine("Seleccionar una opción: ");
 int opcion = int.Parse(Console.ReadLine());
+switch (opcion)
+{
+    case 1:
+        {
+            Console.WriteLine("Activo afectado");
+            Console.WriteLine("1. PC");
+            Console.WriteLine("2. Servidor");
+            Console.WriteLine("3. Base de datos");
+            Console.WriteLine("4. Red");
+            Console.WriteLine("Seleccionar una opción: ");
+            int afectado = int.Parse(Console.ReadLine());
+            switch (afectado)
+            {
+                case 1:
+                    {
+                        break;
+                    }
+                case 2:
+                    {
+                        break;
+                    }
+                case 3:
+                    {
+                        break;
+                    }
+                case 4:
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Opción no válida");
+                    }
+                    break;
+            }
+            break;
+        }
+    case 2:
+        {
+            Console.WriteLine("Activo afectado");
+            Console.WriteLine("1. PC");
+            Console.WriteLine("2. Servidor");
+            Console.WriteLine("3. Base de datos");
+            Console.WriteLine("4. Red");
+            Console.WriteLine("Seleccionar una opción: ");
+            int afectado = int.Parse(Console.ReadLine());
+            switch (afectado)
+            {
+                case 1:
+                    {
+                        break;
+                    }
+                case 2:
+                    {
+                        break;
+                    }
+                case 3:
+                    {
+                        break;
+                    }
+                case 4:
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Opción no válida");
+                    }
+                    break;
+            }
+            break;
+        }
+    case 3:
+        {
+            Console.WriteLine("Activo afectado");
+            Console.WriteLine("1. PC");
+            Console.WriteLine("2. Servidor");
+            Console.WriteLine("3. Base de datos");
+            Console.WriteLine("4. Red");
+            Console.WriteLine("Seleccionar una opción: ");
+            int afectado = int.Parse(Console.ReadLine());
+            switch (afectado)
+            {
+                case 1:
+                    {
+                        break;
+                    }
+                case 2:
+                    {
+                        break;
+                    }
+                case 3:
+                    {
+                        break;
+                    }
+                case 4:
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Opción no válida");
+                    }
+                    break;
+            }
+            break;
+        }
+    case 4:
+        {
+            Console.WriteLine("Activo afectado");
+            Console.WriteLine("1. PC");
+            Console.WriteLine("2. Servidor");
+            Console.WriteLine("3. Base de datos");
+            Console.WriteLine("4. Red");
+            Console.WriteLine("Seleccionar una opción: ");
+            int afectado = int.Parse(Console.ReadLine());
+            switch (afectado)
+            {
+                case 1:
+                    {
+                        break;
+                    }
+                case 2:
+                    {
+                        break;
+                    }
+                case 3:
+                    {
+                        break;
+                    }
+                case 4:
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Opción no válida");
+                    }
+                    break;
+            }
+            break;
+        }
+    default:
+        {
+            Console.WriteLine("Opción no válida");
+        }
+        break;
+}
